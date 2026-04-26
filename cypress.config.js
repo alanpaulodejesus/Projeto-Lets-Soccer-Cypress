ec4f11d83fcd4b3f8061ba1eeb950aa74754fa3c
unknown <apjesus@sefaz.al.gov.br>
2026-04-25 22:35:50 -0300

    ajustes em teste de selecionar formacao

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
     baseUrl: "http://localhost:8080",
+    testIsolation: true, // Obrigatório conforme as dicas do Gist
+    viewportWidth: 1280,
+    viewportHeight: 720,
     setupNodeEvents(on, config) {
-
+      // implement node event listeners here
     },
   },
 });
